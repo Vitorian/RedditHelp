b0273e8a9f198f772862a1abd794449ea00dbe5e
HFTrader <hbucher@gmail.com>
2016-04-05 00:13:11 -0500

    Help to https://www.reddit.com/r/cpp_questions/comments/4dezx6/array_reversal_and_average/

---
+++ ArrayReversalAndAverage.cpp
@@ -0,0 +1,60 @@
+#include <string.h>
+#include <iostream>
+#include <limits> // for NaN()
+
+// Reddit help
+//  https://www.reddit.com/r/cpp_questions/comments/4dezx6/array_reversal_and_average/
+
+// Compile with
+//  g++ ArrayReversalAndAverage.cpp -o ArrayReversalAndAverage
+
+using namespace std;
+
+float avg( int x[], int size )
+{
+    if ( size>0 ) {
+        float total = 0;
+        for (int i = 0; i < size; i++){
+            total += x[i];
+        }
+        return total/size;
+    }
+    return std::numeric_limits<float>::quiet_NaN();
+}
+
+
+
+int main () {
+
+    const unsigned N = 100;
+    int list[N];
+
+    memset( list, 0, sizeof(list) );
+
+    int a = 0;
+    int entries = 0;
+
+    while ( (a!=-1) and (entries<N) ){
+        cout << "Please enter an integer: ";
+        cin >> a;
+        if (a != -1){
+            list[entries]=a;
+            entries++;
+        }
+    }
+
+    // Reverse in place
+    for ( unsigned j=0, i=entries-1; i>j; ++j, --i ) {
+        int tmp = list[j];
+        list[j] = list[i];
+        list[i] = tmp;
+    }
+
+    cout << "The reversed list is ";
+    for (int i = 0; i <= entries-1; i++) {
+        cout << list[i] << " ";
+    }
+
+    cout << "The average of the numbers is "<< avg(list,entries) << "." << endl;
+    return 0;
+}
